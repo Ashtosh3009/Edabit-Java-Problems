a5c34ce2d709885f63ad62da4c5e8e5a4f7dc75e
Ashtosh3009 <86482178+Ashtosh3009@users.noreply.github.com>
2021-09-11 20:43:07 +0530

    Easy

---
+++ Name_Greeting.java
@@ -0,0 +1,20 @@
+/*
+Create a function that takes a name and returns a greeting in the form of a string.
+
+Examples
+helloName("Gerald") ➞ "Hello Gerald!"
+
+helloName("Tiffany") ➞ "Hello Tiffany!"
+
+helloName("Ed") ➞ "Hello Ed!"
+Notes
+The input is always a name (as string).
+Don't forget the exclamation mark!
+*/
+
+public class Challenge {
+	public static String helloName(String name) {
+		String NewName = "Hello " + name + "!";
+		return NewName;
+	}
+}
