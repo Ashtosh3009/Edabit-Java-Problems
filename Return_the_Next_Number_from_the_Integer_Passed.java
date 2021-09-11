0d91577ce9a4d83ea04ab3934f0ebd366a77e4ed
Ashtosh3009 <86482178+Ashtosh3009@users.noreply.github.com>
2021-09-11 18:19:10 +0530

    Very Easy

---
+++ Return_the_Next_Number_from_the_Integer_Passed.java
@@ -0,0 +1,18 @@
+/*
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+Examples
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2
+Notes
+Don't forget to return the result.
+*/
+public class Challenge {
+	public static int addition(int num) {
+		int n = num + 1;
+		return n;
+	}
+}
