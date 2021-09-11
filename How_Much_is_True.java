5abf8e8cebf766d81d697b6f66efae1f232bf1fc
Ashtosh3009 <86482178+Ashtosh3009@users.noreply.github.com>
2021-09-11 18:56:25 +0530

    Easy

---
+++ How_Much_is_True.java
@@ -0,0 +1,22 @@
+/*
+Create a function which returns the number of true values there are in an array.
+
+Examples
+countTrue([true, false, false, true, false]) ➞ 2
+
+countTrue([false, false, false, false]) ➞ 0
+
+countTrue([]) ➞ 0
+Notes
+Return 0 if given an empty array.
+All array items are of the type bool (true or false).
+*/
+import java.util.stream.IntStream;
+
+public class Challenge {
+	public static int countTrue(boolean[] arr) {
+		int coun = 0;
+		for (boolean i : arr) if (i) coun += 1;
+		return coun;
+	}
+}
