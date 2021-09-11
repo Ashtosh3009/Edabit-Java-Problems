fca107b1b73619b4028a4159ab45cbaf3b60d3f0
Ashtosh3009 <86482178+Ashtosh3009@users.noreply.github.com>
2021-09-11 18:24:40 +0530

    Very Easy

---
+++ Are_the_Numbers_Equal_2.java
@@ -0,0 +1,18 @@
+/*Create a function that returns true when x is equal to y; otherwise return false.
+
+Examples
+isSameNum(4, 8) ➞ false
+
+isSameNum(2, 2) ➞  true
+
+isSameNum(42, 32) ➞  false
+Notes
+Don't forget to return the result.*/
+public class Program {
+	public static boolean isSameNum(int x, int y) {
+		if(x == y)
+			return true;
+		else
+			return false;
+	}
+}
